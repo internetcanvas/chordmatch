d81514654d6559ce180e2a930875ee590becd4ba
Brenn Berliner <brenn.berliner@gmail.com>
2023-07-08 16:46:03 -0800

    tunings, chords, faqs, seo

--- static/faqs.js
+++ static/faqs.js
@@ -11,6 +11,17 @@ faqs = [
 	{
 		"q": "How do I use it?",
 		"a": "Click on or tap the circles in the position where your fingers are. You'll see the notes played and the chord name, if any. Remember to include open strings."
+	},
+	{
+		"q": "What chords are supported?",
+		"a": "It currently recognizes 40 different chord qualities. It can also identify slash chords, where you're playing an alternate root note."
+	},
+	{
+		"q": "What instruments are supported?",
+		"a": "Almost any stringed instrument using standard intervals. Choose between 2 to 10 strings and up to 32 frets. Select from common tuning presets or enter your own."
+	},
+	{
+		"q": "What's coming next?",
+		"a": "We're working on saving individual settings, capturing chord progressions, and audio note detection. If you have suggestions, please send them our way."
 	}
-	
 ];
--- static/tunings.js
+++ static/tunings.js
@@ -1,16 +1,22 @@
 
 tunings = {
+	"2": [
+		{"notes": ['e','a'], "name": "Standard"},
+		{"notes": ['d','a'], "name": "Drop D"},
+		{"notes": ['eb','ab'], "name": "1/2 Step Down"},
+		{"notes": ['db','ab'], "name": "1/2 Step Drop Db"}
+	], 
 	"3": [
 		{"notes": ['e','a','d'], "name": "Standard"},
 		{"notes": ['d','a','d'], "name": "Drop D"},
 		{"notes": ['eb','ab','db'], "name": "1/2 Step Down"},
-		{"notes": ['db','ab','db'], "name": "1/2 Step Drop D"}
+		{"notes": ['db','ab','db'], "name": "1/2 Step Drop Db"}
 	], 
 	"4": [
 		{"notes": ['e','a','d','g'], "name": "Standard"},
 		{"notes": ['d','a','d','g'], "name": "Drop D"},
 		{"notes": ['eb','ab','db','gb'], "name": "1/2 Step Down"},
-		{"notes": ['db','ab','db','gb'], "name": "1/2 Step Drop D"},
+		{"notes": ['db','ab','db','gb'], "name": "1/2 Step Drop Db"},
 		{"notes": ['c','g','d','a'], "name": "Cello/Viola"},
 		{"notes": ['g','d','a','e'], "name": "Mandolin/Violin"},
 		{"notes": ['g','c','e','a'], "name": "Ukulele"}
@@ -19,7 +25,7 @@ tunings = {
 		{"notes": ['e','a','d','g','b'], "name": "5 String Standard"},
 		{"notes": ['d','a','d','g','b'], "name": "5 String Drop D"},
 		{"notes": ['eb','ab','db','gb','bb'], "name": "1/2 Step Down"},
-		{"notes": ['db','ab','db','gb','bb'], "name": "1/2 Step Drop D"},
+		{"notes": ['db','ab','db','gb','bb'], "name": "1/2 Step Drop Db"},
 		{"notes": ['b','e','a','d','g'], "name": "5 String Bass"},
 		{"notes": ['g','d','g','b','d'], "name": "5 String Banjo"},
 		{"notes": ['f','c','g','d','a'], "name": "5 String Cello Low"},
@@ -29,17 +35,20 @@ tunings = {
 		{"notes": ['e','a','d','g','b','e'], "name": "Standard"},
 		{"notes": ['d','a','d','g','b','e'], "name": "Drop D"},
 		{"notes": ['eb','ab','db','gb','bb','eb'], "name": "1/2 Step Down"},
-		{"notes": ['db','ab','db','gb','bb','eb'], "name": "1/2 Step Drop D"},
+		{"notes": ['db','ab','db','gb','bb','eb'], "name": "1/2 Step Drop Db"},
 		{"notes": ['b','e','a','d','f#','b'], "name": "Baritone"},
 		{"notes": ['a','e','a','d','f#','b'], "name": "Baritone Drop A"},
-		{"notes": ['b','e','a','d','g','c'], "name": "6 String Bass"},
+		{"notes": ['f#','b','e','a','d','g'], "name": "6 String Bass Low"},
+		{"notes": ['b','e','a','d','g','c'], "name": "6 String Bass High"},
 		{"notes": ['f','c','g','d','a','e'], "name": "6 String Cello"}
 	], 
 	"7": [
 		{"notes": ['b','e','a','d','g','b','e'], "name": "7 String Standard"},
 		{"notes": ['a','e','a','d','g','b','e'], "name": "7 String Drop A"},
 		{"notes": ['bb','eb','ab','db','gb','bb','eb'], "name": "1/2 Step Down"},
-		{"notes": ['ab','eb','ab','db','gb','bb','eb'], "name": "1/2 Step Drop A"},
+		{"notes": ['ab','eb','ab','db','gb','bb','eb'], "name": "1/2 Step Drop Ab"},
+		{"notes": ['f#','b','e','a','d','f#','b'], "name": "7 String Baritone"},
+		{"notes": ['e','a','e','a','d','f#','b'], "name": "7 String Baritone Drop E"},
 		{"notes": ['e','a','d','g','b','e','a'], "name": "7 String High"}
 	], 
 	"8": [
@@ -48,5 +57,17 @@ tunings = {
 		{"notes": ['f','bb','eb','ab','db','gb','bb','eb'], "name": "1/2 Step Down"},
 		{"notes": ['eb','bb','eb','ab','db','gb','bb','eb'], "name": "1/2 Step Drop Eb"},
 		{"notes": ['b','e','a','d','g','b','e','a'], "name": "8 String High"}
+	], 
+	"9": [
+		{"notes": ['c#','f#','b','e','a','d','g','b','e'], "name": "9 String Standard"},
+		{"notes": ['b','e','b','e','a','d','g','b','e'], "name": "9 String Drop B"},
+		{"notes": ['c','f','bb','eb','ab','db','gb','bb','eb'], "name": "1/2 Step Down"},
+		{"notes": ['bb','f','bb','eb','ab','db','gb','bb','eb'], "name": "1/2 Step Drop Bb"}
+	], 
+	"10": [
+		{"notes": ['g#','c#','f#','b','e','a','d','g','b','e'], "name": "10 String Standard"},
+		{"notes": ['f#','b','e','b','e','a','d','g','b','e'], "name": "10 String Drop F#"},
+		{"notes": ['g','c','f','bb','eb','ab','db','gb','bb','eb'], "name": "1/2 Step Down"},
+		{"notes": ['f','c','f','bb','eb','ab','db','gb','bb','eb'], "name": "1/2 Step Drop F"}
 	]
 };
